9f44dc26442778bba63bb3917a03deb73a40551e
nadeshiko582 <sutaron582@gmail.com>
2025-05-10 11:01:19 +0900

    fix;モーダルのサンプル作成

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,6 +18,8 @@
                     ['url' => '/save', 'title' => '保存', 'description' => 'データを保存します', 'icon' => 'save'],
                     ['url' => '/video', 'title' => '動画再生', 'description' => '動画を再生します', 'icon' => 'play'],
                     ['url' => '/cron', 'title' => 'バッチ処理', 'description' => 'バッチ処理を実行します', 'icon' => 'play'],
+                    ['url' => '/modal', 'title' => 'モーダル', 'description' => 'モーダルダイアログの学習', 'icon' => 'modal'],
+                    ['url' => '/sample', 'title' => 'サンプル', 'description' => 'サンプル機能のデモ', 'icon' => 'sample'],
                 ];
                 @endphp
 
@@ -54,6 +56,15 @@
                                     @elseif ($item['icon'] === 'key')
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z" />
+                                        </svg>
+                                    @elseif ($item['icon'] === 'modal')
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 5a1 1 0 011-1h14a1 1 0 011 1v14a1 1 0 01-1 1H5a1 1 0 01-1-1V5z" />
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 8h8v8H8V8z" />
+                                        </svg>
+                                    @elseif ($item['icon'] === 'sample')
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
                                         </svg>
                                     @endif
                                 </span>
